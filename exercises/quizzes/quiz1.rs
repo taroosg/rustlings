4f006603b567b57bd1992c8c8700a08b3fb90109
taroosg <root@taroosg.dev>
2024-11-09 21:40:07 +0900

    done quiz01

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,6 +12,14 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+fn calculate_price_of_apples(amount: i32) -> i32 {
+    if amount <= 40 {
+        amount * 2
+    } else {
+        amount
+    }
+}
+
 fn main() {
     // You can optionally experiment here.
 }
