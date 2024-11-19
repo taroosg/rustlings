084573d2f7491834bfa67b6f20a536cfc712f5f2
taroosg <root@taroosg.dev>
2024-11-19 21:53:03 +0900

    done 11

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -27,11 +27,15 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Lychee,
         Fruit::Pineapple,
     ];
+    // basketをログに出す
 
     for fruit in fruit_kinds {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        if !basket.contains_key(&fruit) {
+            basket.insert(fruit, 1);
+        }
     }
 }
 
