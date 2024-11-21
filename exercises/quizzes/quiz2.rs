728024bede08b05c1910f07308853f3215eafb99
taroosg <root@taroosg.dev>
2024-11-21 11:28:18 +0900

    done quiz2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,16 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        return input
+            .iter()
+            .map(|(text, command)| match *command {
+                Command::Uppercase => text.to_uppercase(),
+                Command::Trim => text.trim().to_string(),
+                Command::Append(n) => format!("{}{}", text, "bar".repeat(n)),
+            })
+            .collect();
+    }
 }
 
 fn main() {
@@ -37,8 +46,8 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
